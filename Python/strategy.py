9bdd828a4c48a5fdc333a1228a34960744950f75
Joaquin Hernandez Martinez <proyecto_noether@outlook.com>
2024-02-09 16:55:10 +0100

    Created strategy pattern in Python

--- Python/strategy.py
+++ Python/strategy.py
@@ -1,24 +1,92 @@
 import abc
+from numbers import Number
+from typing import Sequence
 
 # 1 - Create the interface for the strategies 
 
 class IStrategy(abc.ABC):
     @abc.abstractmethod
-    def algorithm(self):
+    def sorting_algorithm(self):
         ...
         
 # 2 - Create the strategies
 
-
+class SelectionSort(IStrategy):
+    def sorting_algorithm(self, data):
+        return ...
+    
+class BucketSort(IStrategy):
+    def sorting_algorithm(self, data):
+        return ...
+    
+class HeapSort(IStrategy):
+    def sorting_algorithm(self, data):
+        return ...
+    
+class QuickSort(IStrategy):
+    def sorting_algorithm(self, data):
+        return ...
+    
+class RadixSort(IStrategy):
+    def sorting_algorithm(self, data):
+        return ...
+    
+class MergeSort(IStrategy):
+    def sorting_algorithm(self, data):
+        return ...
+    
+class TimSort(IStrategy):
+    def sorting_algorithm(self, data):
+        return ...
 
 # 3 - Create the context
 
-
+class Sorter:
+    def __init__(self, strategy: IStrategy, data: Sequence[Number] = None):
+        self._strategy = strategy
+        self._data = data
+    
+    @property
+    def strategy(self) -> IStrategy:
+        return self._strategy
+    
+    @strategy.setter
+    def strategy(self, strategy: IStrategy):
+        self._strategy = strategy
+        
+    def sort(self, data: Sequence[Number] = None) -> Sequence[Number]:
+        if data:
+            self._data = data
+        result = self._strategy.sorting_algorithm(self._data)
+        self._data = result  # Not needed, but I like to keep the sorted data since it's already sorted
+        return result
+    
+    def __str__(self) -> str:
+        return f"Sorter -> Strategy: {self._strategy.__class__.__name__}  Data: {self._data}"
 
 # 4 - Use the pattern
 
+unsorted_list = [2, 5, 1, 3, 1, -2, 4]  # Sample data
 
+sorter_instance = Sorter()  # Instantiation of a Sorter
+sorter_instance.sort(unsorted_list)  # Use of the strategy pattern
+sorter_instance.strategy = QuickSort()  # Change of algorithm during runtime
+other_instance = Sorter(MergeSort(), unsorted_list)  # Sorter can be instantiated as many times as needed
+other_instance.sort()  # It's a good practice to have default behaviour
 
 
 if __name__ == "__main__":
-    print("hey")
+    test_data = [0, -11, 3.2, 60, 29, 96, -19, 8, 19, 7.1, 48, 58, 2.8, -81, 24, 84, 5.0, 9, 29, 103.462, 8,
+                   60, 8.2, 34, 85, 100, -5.2, 28, 60, 88, 76, 8, 30.42, 79, -22, 40, 54, 0, 18, 89, -43.0031,
+                   59, 71, 70.49, 45, 63, 63, 51, 71, 38, 48, 58, 24, 87, 79, 26, 51, 43, 22, 11, 14, 19, 85]
+    # Instantiation test
+    test = Sorter(data=test_data)
+    print(test)
+    # Algorithm test
+    print(Sorter(BucketSort(), test_data))
+    print(Sorter(HeapSort(), test_data))
+    print(Sorter(QuickSort(), test_data))
+    print(Sorter(RadixSort(), test_data))
+    print(Sorter(MergeSort(), test_data))
+    print(Sorter(TimSort(), test_data))
+    
