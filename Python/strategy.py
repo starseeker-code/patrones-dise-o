fa0a05f1c3167d0adae2e87f99182b4774f57159
Joaquin Hernandez Martinez <proyecto_noether@outlook.com>
2024-02-09 13:09:31 +0100

    Created basic strategy structure

---
+++ Python/strategy.py
@@ -0,0 +1,24 @@
+import abc
+
+# 1 - Create the interface for the strategies 
+
+class IStrategy(abc.ABC):
+    @abc.abstractmethod
+    def algorithm(self):
+        ...
+        
+# 2 - Create the strategies
+
+
+
+# 3 - Create the context
+
+
+
+# 4 - Use the pattern
+
+
+
+
+if __name__ == "__main__":
+    print("hey")
